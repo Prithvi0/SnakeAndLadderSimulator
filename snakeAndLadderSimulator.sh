a5789ecb0175030fb97ff62deffbc0e71987429e
Prithvi0 <prithviy96@gmail.com>
2020-03-09 19:45:52 +0530

    Added program to check options for No play, ladder and snake.

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,6 +1,33 @@
 #!/bin/bash -x
 echo "Welcome to Snakes and Ladders Simulator"
-# PLAY GAME WITH SINGLE PLAYER AT START POSITION 0
+
+# CONSTANTS
+noPlay=0
+ladder=1
+snake=2
+
+# VARIBLES
 position=0
+
 # ROLL A DIE
 dieRoll=$((RANDOM%6+1))
+check=$((RANDOM%3))
+# FUNCTION TO CHECK FOR OPTIONS (NO PLAY, LADDER AND SNAKE)
+function OptionCheck () {
+	case $check in
+		$noPlay )
+			position=0			# NO PLAY
+			;;
+		$ladder )
+			position=$((position+dieRoll))	# LADDER
+			;;
+		$snake )
+			position=$((position-dieRoll))	# SNAKE
+			if [[ $position -lt 0 ]]
+			then
+		         	position=0
+      	fi
+			;;
+	esac
+}
+OptionCheck	# CALLING THE FUNCTION
